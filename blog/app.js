0822e5a28703d1c0784e505382ba2cc8795dca44
jay <jaydeep.w@gmail.com>
2013-03-17 15:13:03 +0530

    A simple bloggin article is working fine, but this one at the moment is not using the DB.

--- blog/app.js
+++ blog/app.js
@@ -3,34 +3,55 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var articleProvider= new ArticleProvider();
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.get('/blog/new', function(req, res) {
+      res.render('blog_new.jade', {
+          title: 'New Post'
+      });
 });
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', 
+          {
+            title: 'Blog',
+            articles:docs
+            });
+    })
+});
+
+app.listen(3000);
+console.log("Server listening on port 3000");
